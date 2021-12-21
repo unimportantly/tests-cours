4c1d9b41b5b2a178762f6a8e92734578a8abbb73
clement <clement.cauliez@gmail.com>
2021-12-21 11:41:31 +0100

    feat: [employee] added controller

---
+++ test_/src/main/java/com/example/test/employee/EmployeeController.java
@@ -0,0 +1,52 @@
+package com.example.test.employee;
+
+import com.example.test.employee.dto.EmployeeDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+@RestController
+@CrossOrigin
+@RequestMapping("employees")
+public class EmployeeController {
+
+    private EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping
+    public List<EmployeeDTO> findAll(){
+        return employeeService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<EmployeeDTO> findById(@PathVariable Long id){
+        try{
+            return ResponseEntity.ok(this.employeeService.findOne(id).get());
+        }
+        catch(NoSuchElementException e) {
+            return ResponseEntity.notFound().header(e.getMessage()).build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<EmployeeDTO> save(@RequestBody EmployeeDTO employeeDTO){
+        return ResponseEntity.ok(this.employeeService.save(employeeDTO));
+    }
+
+    @PutMapping
+    public ResponseEntity<EmployeeDTO> update(@RequestBody EmployeeDTO employeeDTO){
+        return ResponseEntity.ok(this.employeeService.save(employeeDTO));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Boolean> delete(@RequestBody EmployeeDTO employeeDTO){
+        this.employeeService.delete(employeeDTO);
+        return ResponseEntity.ok(true);
+    }
+}
